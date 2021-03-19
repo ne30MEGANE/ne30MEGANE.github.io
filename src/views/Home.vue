877dc64eaf8d66cfcd9fa425ebdc536e2b3b6915
Takahashi <ne300229@senshu-u.jp>
2021-03-19 17:08:32 +0900

    リストのスタイル設定

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,23 @@
       <h3>開発/制作・運営に関わった作品</h3>
     </div>
     <div class="links">
-      <router-link to="/otofuda">対戦型音楽カードゲーム「音札」</router-link>
-      <router-link to="/mugimusya">オリジナルVTuber「天ノ葉つむぎ」「武者丸」</router-link>
-      <router-link to="/">サークル部員交流・情報発信サイト「専修大学電子計算機研究会 公式サイト」</router-link>
+      <router-link to="/otofuda">
+        <div class="item">
+          <b>音札</b><br>対戦型音楽カードゲーム
+        </div>
+      </router-link>
+
+      <router-link to="/mugimusya">
+        <div class="item">
+          <b>天ノ葉つむぎ/武者丸</b><br>オリジナルVTuberによる他メディア展開
+        </div>
+      </router-link>
+
+      <router-link to="/">
+        <div class="item">
+          <b>EDPS公式サイト</b><br>専修大学電子計算機研究会(EDPS)部員交流・情報発信サイト
+        </div>
+      </router-link>
     </div>
 
     <div class="index">
@@ -15,13 +29,47 @@
       <h3>個人で制作した作品</h3>
     </div>
     <div class="links">
-      <router-link to="/">脱出ゲーム「RoomEscape」</router-link>
-      <router-link to="/">タイピングゲーム「ガバタイピングゲーム」</router-link>
-      <router-link to="/">模擬ECサイト「ホビーショップNS」</router-link>
-      <router-link to="/">Webアプリケーション「ENST!! Song Catalog」</router-link>
-      <router-link to="/">ラインエディタ「ED」</router-link>
-      <router-link to="/">自作言語「Sunflower」</router-link>
-      <router-link to="/">CSSフレームワーク「Glasses Frame」</router-link>
+      <router-link to="/">
+        <div class="item">
+          <b>RoomEscape</b><br>脱出ゲーム
+        </div>
+      </router-link>
+
+      <router-link to="/">
+        <div class="item">
+          <b>ガバタイピングゲーム</b><br>タイピングゲーム
+        </div>
+      </router-link>
+
+      <router-link to="/">
+        <div class="item">
+          <b>ホビーショップNS</b><br>模擬ECサイト
+        </div>
+      </router-link>
+      
+      <router-link to="/">
+        <div class="item">
+          <b>ENST!! Song Catalog</b><br>WEBサイト
+        </div>
+      </router-link>
+      
+      <router-link to="/">
+        <div class="item">
+          <b>EDエディタ</b><br>クラシカルなラインエディタ
+        </div>
+      </router-link>
+      
+      <router-link to="/">
+        <div class="item">
+          <b>Sunflower</b><br>プログラミング言語
+        </div>
+      </router-link>
+      
+      <router-link to="/">
+        <div class="item">
+          <b>Glasses Frame</b><br>CSSフレームワーク
+        </div>
+      </router-link>
     </div>
 
     <div class="index">
@@ -29,7 +77,11 @@
       <h3>プログラム以外の作品</h3>
     </div>
     <div class="links">
-      <router-link to="/">キャラクターデザイン「もこ」「めい」</router-link>
+      <router-link to="/">
+        <div class="item">
+          <b>もこ/めい</b><br>キャラクターデザイン
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -99,6 +151,28 @@ export default {
     border-radius: 30px;
     border: 8px dotted $third_color;
     background-color: #ffffff80;
+
+    a{ // router-link
+      text-decoration: none;
+      color: $text_main;
+      font-family: 'M PLUS Rounded 1c', sans-serif;
+      flex-basis: 30%;
+      .item{
+        padding: 1em 1em 2em;
+        >b{
+          font-size: larger;
+          &::before{
+            content: "";
+            display: inline-block;
+            width: 1em;
+            height: 1em;
+            border-radius: 50%;
+            background-color: #A8D8EA;
+            vertical-align: middle;
+          }
+        }
+      }
+    }
   }
 }
 </style>
